6c84d21c08ba3c3b557810c021d74951541e5ba7
Mitchell <mitchell.wm.grenier@gmail.com>
2016-03-19 21:15:43 -0400

    Fixed hardcode

--- main.cc
+++ main.cc
@@ -6,11 +6,11 @@
 
 int main(int argc, char ** argv){
 	int status = 0;
-	std::string root_dir = "/Users/Mitchell/Pictures/Misc/rec/";
+	std::string root_dir;
 	std::string password;
-	//root_dir.clear();
-	//std::cout << "Where can I find my files: ";
-	//std::cin >> root_dir;
+	root_dir.clear();
+	std::cout << "Where can I find my files: ";
+	std::cin >> root_dir;
 	std::cout << "Password: ";
 	SetStdinEcho(false);
 	std::cin >> password;
